43dba11effa2fa8ff82476d1e7ea19072872ed2c
Florian <floriangenz@icloud.com>
2023-12-15 14:55:24 +0100

    added basic Frontend project structure

--- Toolbox/src/app/app-routing.module.ts
+++ Toolbox/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {StartPageComponent} from "./contents/start-page/start-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "**", component: StartPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Toolbox/src/app/app.module.ts
+++ Toolbox/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HeaderComponent} from "./header/header.component";
+import {FooterComponent} from "./footer/footer.component";
+import {StartPageComponent} from "./contents/start-page/start-page.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    StartPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
