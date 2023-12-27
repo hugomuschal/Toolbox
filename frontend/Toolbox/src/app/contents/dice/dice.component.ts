e72b7277ebd7a736e448f2e83205479dd89202ee
Florian <floriangenz@icloud.com>
2023-12-27 14:01:39 +0100

    refactored code

--- frontend/Toolbox/src/app/contents/dice/dice.component.ts
+++ frontend/Toolbox/src/app/contents/dice/dice.component.ts
@@ -9,8 +9,8 @@ export class DiceComponent {
 
   diceAmount: number = 1;
   dice: number[] = new Array(this.diceAmount).fill(0);
-  diceResults: { value: number, amount: number }[] = [];
-  diceRotations: { x: number, y: number }[] = new Array(this.diceAmount).fill({x: 0, y: 0});
+  sortedDiceResults: { value: number, amount: number }[] = new Array(1).fill({value: 1, amount: this.diceAmount});
+  unsortedDiceResults: number[] = new Array(this.diceAmount).fill(1);
   maxRotations: number = 2;
   minRotations: number = 1;
   isOddRoll: boolean = false;
@@ -18,6 +18,7 @@ export class DiceComponent {
 
   rollDice() {
     this.isOddRoll = !this.isOddRoll;
+    this.sortedDiceResults = [];
     let diceElements: HTMLCollectionOf<HTMLElement> = document.getElementsByClassName("dice__dice") as HTMLCollectionOf<HTMLElement>;
     let xRotation: number;
     let yRotation: number;
@@ -37,102 +38,53 @@ export class DiceComponent {
         switch (result) {
           case 1:
             diceElement.style.transform = "rotateY(" + (360 + yRotation - 10) + "deg) rotateX(" + (360 + xRotation - 10) + "deg)";
-            this.diceRotations[i] = {y: 360 + yRotation, x: 360 + xRotation};
+            this.updateResults(1)
+            this.unsortedDiceResults[i] = 1;
             break;
           case 2:
             diceElement.style.transform = "rotateY(" + (270 + yRotation - 10) + "deg) rotateX(" + (360 + xRotation - 10) + "deg)";
-            this.diceRotations[i] = {y: 270 + yRotation, x: 360 + xRotation};
+            this.updateResults(2)
+            this.unsortedDiceResults[i] = 2;
             break;
           case 3:
             diceElement.style.transform = "rotateY(" + (180 + yRotation - 10) + "deg) rotateX(" + (90 + xRotation - 10) + "deg)";
-            this.diceRotations[i] = {y: 180 + yRotation, x: 90 + xRotation};
+            this.updateResults(3)
+            this.unsortedDiceResults[i] = 3;
             break;
           case 4:
             diceElement.style.transform = "rotateY(" + (360 + yRotation - 10) + "deg) rotateX(" + (90 + xRotation - 10) + "deg)";
-            this.diceRotations[i] = {y: 360 + yRotation, x: 90 + xRotation};
+            this.updateResults(4)
+            this.unsortedDiceResults[i] = 4;
             break;
           case 5:
             diceElement.style.transform = "rotateY(" + (90 + yRotation - 10) + "deg) rotateX(" + (360 + xRotation - 10) + "deg)";
-            this.diceRotations[i] = {y: 90 + yRotation, x: 360 + xRotation};
+            this.updateResults(5)
+            this.unsortedDiceResults[i] = 5;
             break;
           case 6:
             diceElement.style.transform = "rotateY(" + (180 + yRotation - 10) + "deg) rotateX(" + (360 + xRotation - 10) + "deg)";
-            this.diceRotations[i] = {y: 180 + yRotation, x: 360 + xRotation};
+            this.updateResults(6)
+            this.unsortedDiceResults[i] = 6;
             break;
         }
       }
     }
   }
 
-  getResult() {
-    this.diceResults = []
-    const xSitesOdd: number[] = [1, 4, 6, 3];
-    const xSitesEven: number[] = [1, 3, 6, 4];
-
-    const ySitesOdd1: number[] = [1, 5, 6, 2];
-    const ySitesOdd4: number[] = [4, 5, 3, 2];
-    const ySitesOdd6: number[] = [6, 5, 1, 2];
-    const ySitesOdd3: number[] = [3, 5, 4, 2];
-
-    const ySitesEven1: number[] = [1, 2, 6, 5];
-    const ySitesEven4: number[] = [4, 2, 3, 5];
-    const ySitesEven6: number[] = [6, 2, 1, 5];
-    const ySitesEven3: number[] = [3, 2, 4, 5];
-
-    for (let i = 0; i < this.diceRotations.length; i++) {
-      let x: number;
-      let result: number = 0;
-      let xIndex: number = this.diceRotations[i].x / 90 % 4;
-      let yIndex: number = this.diceRotations[i].y / 90 % 4;
-
-      if (this.isOddRoll) {
-        x = xSitesOdd[xIndex];
-        switch (x) {
-          case 1:
-            result = ySitesOdd1[yIndex];
-            break;
-          case 4:
-            result = ySitesOdd4[yIndex];
-            break;
-          case 6:
-            result = ySitesOdd6[yIndex];
-            break;
-          case 3:
-            result = ySitesOdd3[yIndex];
-            break;
-        }
-      } else {
-        x = xSitesEven[xIndex * -1];
-        switch (x) {
-          case 1:
-            result = ySitesEven1[yIndex * -1];
-            break;
-          case 4:
-            result = ySitesEven4[yIndex * -1];
-            break;
-          case 6:
-            result = ySitesEven6[yIndex * -1];
-            break;
-          case 3:
-            result = ySitesEven3[yIndex * -1];
-            break;
-        }
-      }
-      this.updateResults(result);
-    }
-    this.sortResults();
-  }
 
   updateResults(result: number) {
-    if (this.diceResults.length == 0) {
-      this.diceResults.push({value: result, amount: 1});
+    if (this.sortedDiceResults.length == 0) {
+      this.sortedDiceResults.push({value: result, amount: 1});
     } else {
-      for (let i: number = 0; i < this.diceResults.length; i++) {
-        if (this.diceResults[i].value == result) {
-          this.diceResults[i].amount++;
+      for (let i: number = 0; i < this.sortedDiceResults.length; i++) {
+        if (this.sortedDiceResults[i].value == result) {
+          this.sortedDiceResults[i].amount++;
           return;
-        } else if (i == this.diceResults.length - 1) {
-          this.diceResults.push({amount: 1, value: result});
+        }else if (this.sortedDiceResults[i].value == result * - 1){
+          this.sortedDiceResults[i].amount--;
+          return;
+        } else if (i == this.sortedDiceResults.length - 1) {
+          this.sortedDiceResults.push({amount: 1, value: result});
           return;
         }
       }
@@ -140,15 +92,15 @@ export class DiceComponent {
   }
 
   sortResults() {
-    this.diceResults.sort((a, b) => a.value - b.value)
+    this.sortedDiceResults.sort((a, b) => a.value - b.value)
   }
 
   addDice() {
     if (this.diceAmount < 100) {
       this.diceAmount++;
       this.dice.push(0);
-      this.diceRotations.push({x: 0, y: 0})
-      this.getResult()
+      this.updateResults(1)
+      this.unsortedDiceResults.push(1)
     }
   }
 
@@ -156,8 +108,8 @@ export class DiceComponent {
     if (this.diceAmount > 1) {
       this.diceAmount--;
       this.dice.splice(this.dice.length - 1, 1);
-      this.diceRotations.splice(this.diceRotations.length - 1, 1);
-      this.getResult()
+      this.updateResults(-this.unsortedDiceResults[this.unsortedDiceResults.length - 1])
+      this.unsortedDiceResults.splice(this.unsortedDiceResults.length - 1, 1);
     }
   }
 }
